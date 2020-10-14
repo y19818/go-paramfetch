f4042ef79ea0c16356a5b76153b6485eec420094
filecash <filecoincash@gmail.com>
2020-10-15 01:39:11 +0800

    filecash/v0.7.0

--- paramfetch.go
+++ paramfetch.go
@@ -23,7 +23,8 @@ import (
 var log = logging.Logger("build")
 
 //const gateway = "http://198.211.99.118/ipfs/"
-const gateway = "https://proofs.filecoin.io/ipfs/"
+// const gateway = "https://proofs.filecoin.io/ipfs/"
+const gateway = "http://118.123.241.59:8080/ipfs/"
 const paramdir = "/var/tmp/filecoin-proof-parameters"
 const dirEnv = "FIL_PROOFS_PARAMETER_CACHE"
 
